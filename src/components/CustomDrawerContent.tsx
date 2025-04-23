9a647d6a5d36680db56bc2c25612d310510113c4
Luna <stasi_kostov01@abv.bg>
2025-04-23 13:32:47 +0200

    Information pages up to 3 done

--- src/components/CustomDrawerContent.tsx
+++ src/components/CustomDrawerContent.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import {
   View,
   Text,
@@ -7,6 +7,8 @@ import {
   Pressable,
   TextStyle,
   Platform,
+  Modal,
+  Dimensions,
 } from 'react-native';
 
 import { AuthContext } from '../context/AuthContext';
@@ -20,10 +22,108 @@ import {
 import { Fonts } from '../styles';
 import Feather from '@expo/vector-icons/Feather';
 import { Avatar } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { topLeft } from '@shopify/react-native-skia';
+import { useSharedValue } from 'react-native-reanimated';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+const windowWidth = Dimensions.get('window').width;
 
 export const CustomDrawerContent = (props) => {
+
   const { user, signOut } = useContext(AuthContext);
   const { navigation } = props;
+
+  const [informatiegidsModalActive, setInformatiegidsModalActive] =
+    useState<boolean>(false);
+
+  const[informatiegidsIndex, setInformatiegidsIndex] = useState<number>(0);
+  const informatiegidsLength = 5;
+
+  const handleNext = () => {
+    if(informatiegidsIndex < informatiegidsLength)
+    {
+      setInformatiegidsIndex((prev) => ++prev);
+    }
+  }
+  const handlePrevious = () => {
+    if(informatiegidsIndex!=0)
+    {
+      setInformatiegidsIndex((prev) => --prev);
+    }
+  }
+  const informatiegidsSteps = [
+    {
+      title: 'Releafe',
+      description: '',
+    },
+    {
+      title: 'Onderdelen',
+      description: 'Deze mobiele applicatie bevat verschillende onderdelen die je kunt gebruiken:',
+    },
+    {
+      title: 'Toolkit',
+      description: 'Binnen de toolkit vind je meerdere tools die je kunt gebruiken:',
+    },
+    {
+      title: 'Bonsaiboom',
+      description: 'Op verschillende plekken kun je Releafe-punten verdienen, bijvoorbeeld door een oefening te doen of een zorg te reframen.',
+    },
+    {
+      title: 'Notificaties',
+      description: 'Binnen Releafe. kun je pushmeldingen ontvangen, bijvoorbeeld wanneer het tijd is om je dagboek in te vullen of een oefening uit te voeren, afhankelijk van hoe jij dit instelt.',
+      icon: require('../../assets/images/bell_icon.png'),
+    },
+    {
+      title: 'Feedback',
+      description: 'Zijn er onduidelijkheden of heb je iets onjuist waargenomen binnen de mobiele applicatie?',
+      icon: require('../../assets/images/information_icon.png'),
+    },
+  ]
+  const informatiegidsNavigationElements = [
+    {
+      icon: require('../../assets/images/navigation_bar/home_icon.png'),
+      boldtext: 'Het hoofdscherm',
+      text: ' biedt een overzicht van je opgeslagen gegevens, zodat je snel een samenvatting van je data kunt inzien.',
+    },
+    {
+      icon: require('../../assets/images/navigation_bar/diary_icon.png'),
+      specialtext: 'In ',
+      boldtext: 'het dagboek',
+      text: ' kun je dagelijks je algehele gevoel en andere factoren rondom je mentale gezondheid bijhouden, evenals het beoordelen van jouw gestelde persoonlijke doelen.',
+    },
+    {
+      icon: require('../../assets/images/navigation_bar/wellbeing_overview_icon.png'),
+      boldtext: 'Het welzijnsoverzicht',
+      text: ' toont je welzijnstoestand zoals beschreven in je dagboek. Ook kun je hier gegevens van andere dagen terugvinden.',
+    },
+    {
+      icon: require('../../assets/images/navigation_bar/toolkit_icon.png'),
+      boldtext: 'De toolkit',
+      text: ' biedt verschillende tools ter ondersteuning van het verbeteren van je mentale gezondheid.'
+    }
+  ];
+  const informatiegidsExerciseElements = [
+    {
+      icon: require('../../assets/images/custom_icons/persoonlijke_doelen_icon.png'),
+      text: 'Bij de persoonlijke doelen kun je doelen stellen in verschillende richtingen, die je kunt personaliseren en waaraan je herinneringen kunt koppelen.'
+    },
+    {
+      icon: require('../../assets/images/custom_icons/zorgenbakje_icon.png'),
+      text: 'In het zorgenbakje kun je tijdelijk je zorgen opbergen, zodat je deze van je af kunt schrijven.',
+    },
+    {
+      icon: require('../../assets/images/custom_icons/reframing_icon.png'),
+      text: 'De eerder omschreven of nieuwe zorgen kun je reframen, zodat je leert op een andere manier naar negatieve gedachten te kijken.',
+    },
+    {
+      icon: require('../../assets/images/custom_icons/berichten_aan_jezelf_icon.png'),
+      text: 'Je kunt berichten voor jezelf uitschrijven als notities, die je later kunt teruglezen en waaraan je media, zoals audio-opnamen, kunt koppelen.',
+    },
+    {
+      icon: require('../../assets/images/custom_icons/oefeningen_icon.png'),
+      text: 'Met onze vele oefeningen kun je aan de slag met mindfulness, meditatie, ontspanning, lichaamsbeweging en ademhaling.',
+    },
+  ]
 
   const menuConfigGroup1 = [
     {
@@ -48,7 +148,11 @@ export const CustomDrawerContent = (props) => {
     {
       label: 'Informatiegids',
       icon: require('../../assets/images/drawer_icons/drawer_info_icon.png'),
-      action: () => console.log('Informatiegids pressed!'),
+      action: () => 
+        {
+          console.log('Informatiegids pressed!');
+          setInformatiegidsModalActive(!informatiegidsModalActive);
+        }
     },
     {
       label: 'Instellingen',
@@ -64,6 +168,208 @@ export const CustomDrawerContent = (props) => {
   ];
 
   return (
+  <>
+    <SafeAreaView>
+      <Modal
+        animationType='none'
+        transparent={true}
+        visible={informatiegidsModalActive}
+        onRequestClose={() =>
+        {
+          setInformatiegidsIndex(0);
+          setInformatiegidsModalActive(!informatiegidsModalActive)
+        }
+        }
+      >
+        <View style={styles.modalWrapper}>
+          <View style={styles.modalContainer}>
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+              }}
+            >
+              {/*First Page*/}
+              {informatiegidsIndex == 0 && (
+              <>
+              <Image source={require('../../assets/images/logo_releafe_05.png')}
+                style={{ width: 88, height: 62, paddingBottom: 90 }}
+                resizeMode='contain'/>
+              <View style={{ rowGap: 20 }}>
+                <Text style={styles.informationTitleIntro}>
+                  Hi {user?.firstName}, en welkom bij Releafe.
+                </Text>
+                <Text style={styles.informationBody}>
+                Releafe. biedt je de mogelijkheid om actief aan je mentale gezondheid te werken en ondersteuning te krijgen bij je 
+                mentale klachten.
+                </Text>
+                <Text style={styles.informationBody}>
+                  Volg deze informatiegids om te ontdekken hoe de mobiele applicatie werkt en wat wij voor jou kunnen betekenen.
+                </Text>
+              </View>
+              </>
+              )}
+              {/*Title, Description & Icon for pages past the first*/}
+              {informatiegidsIndex >= 1 && (
+              <>
+              <View style ={
+                {
+                  display: 'flex',
+                  flexDirection: 'row',
+                  marginBottom: 10,
+                }
+              }>
+                {informatiegidsSteps[informatiegidsIndex].icon && (
+                  <Image style= {
+                  {
+                    objectFit: 'contain',
+                    paddingRight: 10,
+                    paddingLeft: 10,
+                    height: 20,
+                    width: 20,
+                    marginRight: 5,
+                    top: 4,
+                  }
+                  }
+                  source={informatiegidsSteps[informatiegidsIndex].icon}></Image>
+                  )}
+                <Text style = {styles.informationTitle}>
+                  {informatiegidsSteps[informatiegidsIndex].title}
+                </Text>
+              </View>
+              <Text style = {{...styles.informationBody, marginBottom: 10}}>
+                {informatiegidsSteps[informatiegidsIndex].description}
+              </Text>
+              </>)}
+              {/*Second Page*/}
+              {informatiegidsIndex == 1 && (
+                <>
+                {Array.from({ length: informatiegidsNavigationElements.length}).map((_, index) => {
+                  return (
+                  <View 
+                  key = {index}
+                  style= {{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    marginBottom: 10,
+                    marginRight: 30,
+                    }}>
+                    <Image source={informatiegidsNavigationElements[index].icon} style = {styles.navigationScreenIcon}></Image>
+                    {informatiegidsNavigationElements[index].specialtext ? (
+                    <Text style={styles.informationBody}>
+                      {informatiegidsNavigationElements[index].specialtext}
+                      <Text style = {styles.informationBodyBold}>
+                        {informatiegidsNavigationElements[index].boldtext}
+                        <Text style = {styles.informationBody}>
+                          {" "}{informatiegidsNavigationElements[index].text}
+                        </Text>
+                      </Text>
+                    </Text>
+                    ) : 
+                      <Text style = {styles.informationBodyBold}>
+                        {informatiegidsNavigationElements[index].boldtext}
+                        <Text style = {styles.informationBody}>
+                          {" "}{informatiegidsNavigationElements[index].text}
+                        </Text>
+                      </Text>
+                    }
+                  </View>
+                  );
+                  })}
+                </>
+              )}
+              {/*Third Page*/}
+              {informatiegidsIndex == 2 && (
+              <>
+                {Array.from({ length: informatiegidsExerciseElements.length}).map((_, index) => {
+                  return (
+                  <View
+                  key={index} 
+                  style= {{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    marginBottom: 10,
+                    marginRight: 30,
+                    }}>
+                    <Image source={informatiegidsExerciseElements[index].icon} style = {styles.navigationScreenIcon}></Image>
+                    <Text style={styles.informationBody}>
+                      {informatiegidsExerciseElements[index].text}
+                    </Text>
+                  </View>
+                  );
+                  })}
+                </>   
+              )}
+              {/*Fourth Page*/}
+              {informatiegidsIndex == 3 && (
+                <>
+                
+                </>
+              )}
+            </View>
+            {/*Close out Button*/}
+            <Pressable style = {{position: 'absolute', top: 24, right: 24,}}
+              onPress={() =>
+                setInformatiegidsModalActive(!informatiegidsModalActive)
+              }
+            >
+              <Feather name='x-circle' size={24} color='gray' />
+            </Pressable>
+            {/*Navigation controls*/}
+            <View
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      justifyContent: 'space-between',
+                      marginTop: 20,
+                    }}
+                  >
+                    <Pressable
+                      onPress={() => handlePrevious()}
+                      disabled={informatiegidsIndex == 0 ? true : false}
+                      style={informatiegidsIndex == 0 ? { opacity: 0.4 } : {}}
+                    >
+                      <MaterialCommunityIcons
+                        name='chevron-left-circle-outline'
+                        size={27}
+                        color='black'
+                      />
+                    </Pressable>
+            
+                    <View style={{ display: 'flex', flexDirection: 'row', columnGap: 7 }}>
+                      {Array.from({ length: informatiegidsLength + 1 }).map((_, index) => {
+                        return (
+                          <View
+                            key={index}
+                            style={{
+                              width: 8,
+                              height: 8,
+                              backgroundColor: index === informatiegidsIndex ? '#829c7a' : '#E4E1E1',
+                              borderRadius: 99,
+                            }}
+                          ></View>
+                        );
+                      })}
+                    </View>
+            
+                    <Pressable
+                      onPress={() => handleNext()}
+                      disabled={informatiegidsIndex == informatiegidsLength  ? true : false}
+                      style={informatiegidsIndex == informatiegidsLength  ? { opacity: 0.4 } : {}}
+                    >
+                      <MaterialCommunityIcons
+                        name='chevron-right-circle-outline'
+                        size={27}
+                        color='black'
+                      />
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </SafeAreaView>
     <View style={{ flex: 1 }}>
       <View style={styles.headersContainer}>
         <View
@@ -182,6 +488,7 @@ export const CustomDrawerContent = (props) => {
         </View>
       </DrawerContentScrollView>
     </View>
+   </>
   );
 };
 
@@ -212,6 +519,30 @@ const styles = StyleSheet.create({
     color: 'black',
     fontSize: 16,
   } as TextStyle,
+  informationTitleIntro:
+  {
+    ...Fonts.sofiaProMedium[Platform.OS],
+    color: 'black',
+    fontSize: 20,
+  } as TextStyle,
+  informationTitle:
+  {
+    ...Fonts.sofiaProSemiBold[Platform.OS],
+    color: 'black',
+    fontSize: 18,
+  } as TextStyle,
+  informationBody:
+  {
+    ...Fonts.sofiaProLight[Platform.OS],
+    color: 'black',
+    fontSize: 14,
+  } as TextStyle,
+  informationBodyBold:
+  {
+    ...Fonts.sofiaProSemiBold[Platform.OS],
+    color: 'black',
+    fontSize: 14,
+  } as TextStyle,
   drawerItem: {
     paddingVertical: 10,
   },
@@ -219,4 +550,30 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: '#00000033',
   },
+  modalWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.7)',
+  },
+  modalContainer: {
+    borderRadius: 30,
+    width: windowWidth - 2 * 15,
+    backgroundColor: 'white',
+    padding: 25,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  navigationScreenIcon:
+  {
+    objectFit: 'contain',
+    paddingRight: 10,
+    paddingLeft: 10,
+    height: 26,
+    width: 26,
+    marginRight: 25,
+    top: 4,
+    alignSelf: 'center'
+  }
 });
